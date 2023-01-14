5d20acb553920e35527236cac178c8494944ee45
dragonwarrior1227 <vinay2210978@gmail.com>
2023-01-14 15:36:28 +0100

    firefox

--- ver_fullsetup_firefox.py
+++ ver_fullsetup_firefox.py
@@ -152,20 +152,20 @@ if len(driver.window_handles)>1:
             driver.switch_to.window(driver.window_handles[-1])
             time.sleep(13)
             print("!!!!!111")
-            driver.save_screenshot("viewgrip.png")
-            upload_basic("viewgrip.png",'13ALQG3rJgrQXZxivxKZ_xXED-nInKsnM')
-            time.sleep(1300)
+            # driver.save_screenshot("viewgrip.png")
+            # upload_basic("viewgrip.png",'13ALQG3rJgrQXZxivxKZ_xXED-nInKsnM')
+            time.sleep(13) #1300
         else:
             driver.switch_to.window(window_after)
             driver.set_window_size(403,686)
             driver.execute_script("""document.querySelectorAll("span[onclick='javascript:StartWorker();']")[0].click()""")
             time.sleep(13)
             print("::::::2222")
-            driver.save_screenshot("viewgrip.png")
-            upload_basic("viewgrip.png",'13ALQG3rJgrQXZxivxKZ_xXED-nInKsnM')
-            time.sleep(180)
+            # driver.save_screenshot("viewgrip.png")
+            # upload_basic("viewgrip.png",'13ALQG3rJgrQXZxivxKZ_xXED-nInKsnM')
+            time.sleep(18) #180
             if len(driver.window_handles)>2:
-                time.sleep(1300)
+                time.sleep(13) # 1300
 
 
         driver.switch_to.window(window_after)
